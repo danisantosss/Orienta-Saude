f42733353de7552d0e7a3b964e6ec158c786eda3
danisantosss <danisantos5075@gmail.com>
2026-08-11 13:36:59 -0300

    feat: inicializa estrutura do backend Spring Boot

---
+++ backend/src/main/java/com/orientasaude/OrientaSaudeApplication.java
@@ -0,0 +1,12 @@
+package com.orientasaude;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientaSaudeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrientaSaudeApplication.class, args);
+    }
+}
